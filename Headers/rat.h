062e4c7605585ce110c5e56a9e3f332476df6dc0
cashcraf <ca242521@ohio.edu>
2023-10-18 23:32:25 -0400

    new format

--- Headers/rat.h
+++ Headers/rat.h
@@ -7,16 +7,35 @@
  
 #ifndef RAT
 #define RAT
-#include<iostream>
-#include<string>
+#include "raylib.h"
+#include "animation.h"
+#include "camera.h"
+#include "waffle.h"
+
 using namespace std;
  
-class RAT
+class Rat
 {
 public:
 //your code here (constructors getters setters and function headers)
- 
+Rat(Vector2 position, float width, float height); // constructor
+
+Rectangle GetPosition(){return position;}
+float GetWidth(){return width;}
+float GetHeight(){return height;}
+
+
+void Update();
+bool Dead();
+
+
 private:
- 
+    Rectangle position;
+    float width;
+    float height;
+    SpriteAnimation rat_animation; // enemy rats updated animation
+    SpriteAnimation ratWalk;
+    Texture2D rat = LoadTexture("Images/enemy_rat.png");
+    const float ratSize = 32;
 };
 #endif
---
+++ Headers/waffle.h
@@ -0,0 +1,82 @@
+/**
+*  @file: waffle.h
+*  @author: cashcraf
+*  @date: 2023-10-19
+*  @brief: 
+*/
+ 
+#ifndef WAFFLE
+#define WAFFLE
+
+
+#include "raylib.h"
+#include <stdlib.h>
+#include "animation.h"
+#include "camera.h"
+using namespace std;
+ 
+class Waffle
+{
+public:
+//your code here (constructors getters setters and function headers)
+    Waffle(Vector2 wafflePosition); // constructor
+
+    // initilizers (setters)
+    void initializeAnimations();
+    Vector2 setCameraTarget();
+
+    //getters
+    Camera2D getWafflesCamera() {return camera;}
+    Vector2 getWafflePos(){return wafflePos;}
+
+    // updaters
+    void Update();
+    void UpdateKeysAndAnimations();
+    void UpdatePhysics();
+    void doAnimations();
+
+
+private:
+    // animation values
+    SpriteAnimation waffle_animation; // waffles updated animation
+    SpriteAnimation idleAnimation;
+    SpriteAnimation walkingForwardAnimation;
+    SpriteAnimation walkingBackwardAnimation;
+    SpriteAnimation jumpingAnimationRight;
+    SpriteAnimation jumpingAnimationLeft;
+    SpriteAnimation hissingAnimation; // maybe make this left and right really dont know if I wanna do that
+    Texture2D waffle = LoadTexture("Images/Waffle_Sprite_Sheet.png");
+
+    // screen values
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    // camera values
+    Camera2D camera = InitiateCamera((Vector2){wafflePos.x + waffleSize * scale / 2, wafflePos.y + waffleSize * scale / 2}, (float)screenWidth, (float)screenHeight);
+
+    // waffle postion values
+    Vector2 wafflePos;
+    const float waffleSize = 32; // size of how long and high the sprite is
+    float waffle_index = 0; // which animation waffle is on
+    float scale = 4.0f; // scales the sprite to 4 times its size
+    bool canJump = 1;
+    bool isMoving = 0;
+    bool isJumping = 0;
+    bool isRight = 1;
+    bool isHissing = 0;
+    bool isLeft = 0;
+
+
+    // Physics
+    int jumpForce = -8; // raylib coordinate system flipped, 0,0 is top left instead of bottom left so every value is flipped from what you think. which fine but its weird
+    const float gravity = 0.4;
+    float x_velocity = 5;
+    float y_velocity = 0; // because of the origin being 0,0 being the top left
+
+    // Sound
+    Sound meow = LoadSound("");
+    Sound hiss = LoadSound("Sounds/hissing.wav");
+
+
+};
+#endif
