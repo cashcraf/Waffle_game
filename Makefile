9d1eb581911e9966d68fe4c02eab2e61f51e8226
cashcraf <ca242521@ohio.edu>
2023-11-03 22:22:15 -0400

    In progress: web build

--- Makefile
+++ Makefile
@@ -2,10 +2,16 @@
 CC = g++
 CXX = x86_64-w64-mingw32-g++
 CFLAGS = -Wall
-LDFLAGS = -I/home/cashcraf/raylib/src -L/home/cashcraf/raylib/src
+LDFLAGS = -I/home/cashcraf/raylib/src/windows -L/home/cashcraf/raylib/src/windows
+LDFLAGSweb = -I/home/cashcraf/raylib/src -I/home/cashcraf/emsdk/upstream/emscripten/system/include/emscripten -L/home/cashcraf/raylib/src
 LIBS_LINUX = -lraylib -lGL -lpthread -ldl -lrt -lX11 -lm
 LIBS_WINDOWS = -lraylib -lopengl32 -lgdi32 -lwinmm -lpthread
+LIBS_WEB = -lraylib -lm
 OPT_FLAGS = -O1  # Adjust optimization level as needed
+
+# Emscripten tools and flags
+EMCC = emcc
+EMFLAGS = -O1
 
 # Source files
 SRC = waffle.cc animation.cc camera.cc rat.cc game.cc main.cc
@@ -13,6 +19,7 @@ SRC = waffle.cc animation.cc camera.cc rat.cc game.cc main.cc
 # Output executables
 OUT_LINUX = waffle.exe
 OUT_WINDOWS = windows.exe
+OUT_HTML = waffleWeb.html
 
 all: $(OUT_LINUX) $(OUT_WINDOWS)
 
@@ -21,6 +28,11 @@ $(OUT_LINUX): $(SRC)
 
 $(OUT_WINDOWS): $(SRC)
 	$(CXX) -o $(OUT_WINDOWS) $^ $(CFLAGS) $(LDFLAGS) $(LIBS_WINDOWS) $(OPT_FLAGS)
+
+web: $(OUT_HTML)
+
+$(OUT_HTML): $(SRC)
+	$(EMCC) $(EMFLAGS) $^ -o $(OUT_HTML) $(LIBS_LINUX) $(LDFLAGSweb) -DPLATFORM_WEB -s USE_GLFW=3 -s USE_WEBGL2=1 --preload-file Sounds --preload-file Images --preload-file Headers -s ASYNCIFY --shell-file /home/cashcraf/raylib/src/shell.html
 
 .PHONY: run clean
 
@@ -32,4 +44,4 @@ linux: $(OUT_LINUX)
 windows: $(OUT_WINDOWS)
 
 clean:
-	rm -f $(OUT_LINUX) $(OUT_WINDOWS)
+	rm -f $(OUT_LINUX) $(OUT_WINDOWS) $(OUT_HTML)
--- main.cc
+++ main.cc
@@ -1,5 +1,5 @@
 /**
-*  @file: waffle.cc
+*  @file: main.cc
 *  @author: cashcraf
 *  @date: 2023-10-16 
 *  @brief: Waffles big day out (a game for sam) main file
@@ -7,7 +7,12 @@
 */
  
 // can now compile with make linux or make windows make clean to clear the executibles and make run to run on linux
+//#define PLATFORM_WEB
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#else
 #include "raylib.h"
+#endif
 #include <stdlib.h>
 #include "Headers/animation.h"
 #include "Headers/camera.h"
@@ -15,42 +20,76 @@
 #include "Headers/waffle.h"
 #include "Headers/game.h"
 
+// play web version with python3 -m http.server 
+// then go to http://localhost:8000/index.html
+
 using namespace std;
-int main() {
-    
-    // Initialize Raylib and your game objects
+
+   // Initialize Raylib and your game objects
     const int screenWidth = 800;
     const int screenHeight = 450;
-    InitWindow(screenWidth, screenHeight, "Waffles Big Day OUt");
-    InitAudioDevice();
+
+
     int framesCounter = 0;
     
-    Game* level1 = new Game(5,1);
-    level1->initializeGame();
+    Game* level1 = nullptr;
 
-    // Game level2(5,2);
-    // level2.initializeGame();
+    Game* level2 = nullptr;
+
+    Game* level3 = nullptr;
     
 
     // boolean values for screens
     bool logo = true; // logo screen
     bool intro = false;
     bool game = false;
+
     bool level1On = true;
     bool level1Restart = false;
     bool level1Win = false;
+
     bool level2On = false;
-    
+    bool level2Restart = false;
+    bool level2Win = false;
+
+    bool level3On = false;
+    bool level3Restart = false;
+    bool level3Win = false;
 
     // animation stuff
-    Texture2D titlePic = LoadTexture("Images/title.png");
-    Texture2D logoPic = LoadTexture("Images/logo.png");
+    Texture2D titlePic;
+    Texture2D logoPic;;
+
+// For the web
+void UpdateDrawFrame(void);   
+
+int main() { 
+    
+    InitWindow(screenWidth, screenHeight, "Waffles Big Day OUt");
+    InitAudioDevice();
+    level1 = new Game(5,1);
+    level1->initializeGame();
+
+    Game* level2 = new Game(5,2);
+    level2->initializeGame();
+
+    Game* level3 = new Game(5,3);
+    level3->initializeGame();
+
+    // animation stuff
+    titlePic = LoadTexture("Images/title.png");
+    logoPic = LoadTexture("Images/logo.png");
+
 
 
     // fps
     SetTargetFPS(60);
 
+    #ifdef PLATFORM_WEB
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 
+    #else
+    
     // Set up the game loop
     while (!WindowShouldClose()) {
         if (logo){
@@ -100,6 +139,7 @@ int main() {
                     if (framesCounter >= 120) // wait 2 seconds then reset the level
                     {
                     // Delete the existing level1 and create a new one
+                    level1->cleanUp();
                     delete level1;
                     level1 = new Game(5, 1);
                     level1->initializeGame();
@@ -113,25 +153,258 @@ int main() {
                 level1Win = level1->checkWin(); // checks if waffle has won
 
                 if (level1Win) {
+                    framesCounter++;
+                    if (framesCounter >= 120) // wait 2 seconds then win the level
+                    {
+                        level1->cleanUp();
+                        delete level1;
+                        level2On = true;
+                        level1On = false;
+                        framesCounter = 0;
+                    }
+                }
+            }
+
+
+            else if (level2On){
+                level2->updateGame();
+                level2->drawGame();
+
+                if (!level2Win){
+                level2Restart = level2->checkLose();
+                }
+
+                if (level2Restart && !level2Win) {
+                    framesCounter++;
+                    if (framesCounter >= 120) // wait 2 seconds then reset the level
+                    {
+                        // Delete the existing level1 and create a new one
+                        delete level2;
+                        level2 = new Game(5, 2);
+                        level2->initializeGame();
+                        level2Restart = false;
+                        framesCounter = 0;
+                    }
+                    
+                }
+                
+    
+                level2Win = level2->checkWin(); // checks if waffle has won
+
+                if (level2Win) {
                      framesCounter++;
                      if (framesCounter >= 120) // wait 2 seconds then win the level
                      {
-                    delete level1;
-                    level2On = true;
-                    level1On = false;
+                    level2->cleanUp();
+                    delete level2;
+                    level3On = true;
+                    level2On = false;
                     framesCounter = 0;
                     }
                 }
             }
-            else if (level2On){
-                 // same as level 1 
-                 CloseWindow(); // temporary
-             }
 
+            else if (level3On){
+                level3->updateGame();
+                level3->drawGame();
+
+                if (!level3Win){
+                level3Restart = level3->checkLose();
+                }
+
+                if (level3Restart && !level3Win) {
+                    framesCounter++;
+                    if (framesCounter >= 120) // wait 2 seconds then reset the level
+                    {
+                        // Delete the existing level1 and create a new one
+                        delete level3;
+                        level3 = new Game(5, 3);
+                        level3->initializeGame();
+                        level3Restart = false;
+                        framesCounter = 0;
+                    }
+                    
+                }
+                
+    
+                level3Win = level3->checkWin(); // checks if waffle has won
+
+                if (level3Win) {
+                     framesCounter++;
+                     if (framesCounter >= 120) // wait 2 seconds then win the level
+                     {
+                    level2->cleanUp();
+                    delete level3;
+                    //level4On = true; // dont know if I want a level 4 or a winning screen or to quit yet
+                    level3On = false;
+                    framesCounter = 0;
+                    }
+                }
+            }
+             
 
     }
     }
     CloseWindow();
     CloseAudioDevice();
+    #endif
     return 0;
 }
+
+#ifdef PLATFORM_WEB
+// One iteration of main loop of my game for web
+void UpdateDrawFrame() {
+        if (logo){
+        BeginDrawing();
+        
+        ClearBackground(WHITE);
+        DrawTexture(logoPic, 0, 0, WHITE);
+
+        EndDrawing();
+
+        framesCounter++;    // Count frames
+
+            // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+            if (framesCounter > 120)
+            {
+             // Exit the title screen after 2 seconds
+            logo = false;
+            intro = true;
+            framesCounter = 0;
+        }
+        }
+
+    else if (intro) {
+        ClearBackground(RAYWHITE);
+
+        DrawTexture(titlePic, 0, 0, WHITE);
+        EndDrawing();
+
+        if (IsKeyPressed(KEY_SPACE)) {
+            intro = false;
+            game = true;
+        }
+    }
+
+
+        else if (game) {
+            if (level1On) {
+                level1->updateGame();
+                level1->drawGame();
+
+                if (!level1Win){
+                level1Restart = level1->checkLose();
+                }
+
+                if (level1Restart && !level1Win) {
+                    framesCounter++;
+                    if (framesCounter >= 120) // wait 2 seconds then reset the level
+                    {
+                    // Delete the existing level1 and create a new one
+                    level1->cleanUp();
+                    delete level1;
+                    level1 = new Game(5, 1);
+                    level1->initializeGame();
+                    level1Restart = false;
+                    framesCounter = 0;
+                    }
+                    
+                }
+                
+    
+                level1Win = level1->checkWin(); // checks if waffle has won
+
+                if (level1Win) {
+                    framesCounter++;
+                    if (framesCounter >= 120) // wait 2 seconds then win the level
+                    {
+                        level1->cleanUp();
+                        delete level1;
+                        level2On = true;
+                        level1On = false;
+                        framesCounter = 0;
+                    }
+                }
+            }
+
+
+            else if (level2On){
+                level2->updateGame();
+                level2->drawGame();
+
+                if (!level2Win){
+                level2Restart = level2->checkLose();
+                }
+
+                if (level2Restart && !level2Win) {
+                    framesCounter++;
+                    if (framesCounter >= 120) // wait 2 seconds then reset the level
+                    {
+                        // Delete the existing level1 and create a new one
+                        delete level2;
+                        level2 = new Game(5, 2);
+                        level2->initializeGame();
+                        level2Restart = false;
+                        framesCounter = 0;
+                    }
+                    
+                }
+                
+    
+                level2Win = level2->checkWin(); // checks if waffle has won
+
+                if (level2Win) {
+                     framesCounter++;
+                     if (framesCounter >= 120) // wait 2 seconds then win the level
+                     {
+                    level2->cleanUp();
+                    delete level2;
+                    level3On = true;
+                    level2On = false;
+                    framesCounter = 0;
+                    }
+                }
+            }
+
+            else if (level3On){
+                level3->updateGame();
+                level3->drawGame();
+
+                if (!level3Win){
+                level3Restart = level3->checkLose();
+                }
+
+                if (level3Restart && !level3Win) {
+                    framesCounter++;
+                    if (framesCounter >= 120) // wait 2 seconds then reset the level
+                    {
+                        // Delete the existing level1 and create a new one
+                        delete level3;
+                        level3 = new Game(5, 3);
+                        level3->initializeGame();
+                        level3Restart = false;
+                        framesCounter = 0;
+                    }
+                    
+                }
+                
+    
+                level3Win = level3->checkWin(); // checks if waffle has won
+
+                if (level3Win) {
+                     framesCounter++;
+                     if (framesCounter >= 120) // wait 2 seconds then win the level
+                     {
+                    level2->cleanUp();
+                    delete level3;
+                    //level4On = true; // dont know if I want a level 4 or a winning screen or to quit yet
+                    level3On = false;
+                    framesCounter = 0;
+                    }
+                }
+            }
+             
+
+    }
+}
+#endif
